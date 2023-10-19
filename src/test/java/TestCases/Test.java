a4a9b9f2dbbf88002690f95c51ffdbc3dd250456
Prathmesh Laxmikant Kulkarni <Prathmesh.Kulkarni@multichoice.co.za>
2023-10-19 22:12:10 +0530

    new implementation

--- src/test/java/TestCases/Test.java
+++ src/test/java/TestCases/Test.java
@@ -4,8 +4,12 @@ package TestCases;
 import Utilities.BaseClass;
 import Utilities.DataParser;
 import Utilities.DataReader;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 import java.io.IOException;
@@ -14,12 +18,13 @@ import java.nio.file.Path;
 
 import static io.restassured.RestAssured.*;
 
-public class Test extends BaseClass {
+public class Test extends BaseClass{
     String token;
 
-    @BeforeTest
+    @org.testng.annotations.Test
     public void TokenGeneration() throws IOException {
 
+         ExtentTest test =extent.createTest(Thread.currentThread().getStackTrace()[1].getMethodName());
         RestAssured.baseURI = DataReader.ConfigFileReader().getProperty("endPoint1");
        String response=given().log().all().header("Content-Type","application/json").header("Host","mahauthorizationapi.azurewebsites.net").body("{\n" +
                        "    \"PlatformName\": \"WHATSAPP\",\n" +
@@ -27,6 +32,9 @@ public class Test extends BaseClass {
                        "}")
                .when().post(DataReader.ConfigFileReader().getProperty("resource1")).then().log().all().assertThat()
                .statusCode(200).extract().response().asString();
+        var responseNode=test.createNode("Response Data");
+        responseNode.info(response);
+
        JsonPath js= DataParser.JsonParsing(response);
        this.token=js.getString("Token");
        System.out.println(token);
@@ -47,5 +55,7 @@ public class Test extends BaseClass {
                "  \"smartCardNumber\": \"8061164508\"\n" +
                "}").when().post(DataReader.ConfigFileReader().getProperty("resource2")).then().log().all().assertThat().statusCode(200);
     }
+
+
 
 }
--- src/test/java/Utilities/BaseClass.java
+++ src/test/java/Utilities/BaseClass.java
@@ -2,14 +2,23 @@ package Utilities;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+
+import java.nio.file.FileSystems;
 
 public class BaseClass {
-    static ExtentTest test;
-    static ExtentReports extent;
-    @BeforeClass
-    public void Setup(){
-
+    public ExtentReports extent;
+    @BeforeTest
+    public void BeforeTestCase(){
         extent = new ExtentReports();
+        ExtentSparkReporter spark=new ExtentSparkReporter(FileSystems.getDefault().getPath("").toAbsolutePath() +"\\src\\test\\java\\Report\\index.html");
+        extent.attachReporter(spark);
+    }
+    @AfterTest
+    public void AfterTestCase(){
+        extent.flush();
     }
 }
